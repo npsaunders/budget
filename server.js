ee9b529c3bf25a30ed6e9aa15cf2d867b536c03e
npsaunders <neilpsaunders@gmail.com>
2022-08-31 18:16:15 -0400

    created new.ejs page with html

--- server.js
+++ server.js
@@ -4,17 +4,32 @@ const app = express();
 const port = 3000;
 
 // Routes
+
+// Index
 app.get("/budgets", (req, res) => {
   res.render("index.ejs", {
     budget: budgets
   })
 })
 
+// N
+app.get("/budgets/new", (req, res) => {
+  res.render("new.ejs")
+})
+
+// D
+// U
+// C
+// E
+
+// Show
 app.get("/budgets/:indexBudget", (req, res) => {
   res.render("show.ejs", {
     budget: budgets[req.params.indexBudget]
   })
 })
+
+
 
 // Listener
 app.listen(port, () => {
